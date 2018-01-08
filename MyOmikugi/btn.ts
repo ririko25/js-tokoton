890eb0e86db48aa733fedb38851758fd0a29f03c
ririko <ririkokanikama25@yahoo.co.jp>
2018-01-08 18:59:16 +0900

    ドットインストールおみくじ#9まで

--- MyOmikugi/btn.ts
+++ MyOmikugi/btn.ts
@@ -7,8 +7,18 @@
   "use strict";
   const btn = document.getElementById("btn");
   btn.addEventListener("click", function() {
-    var n = Math.floor(Math.random() * 3);
-    this.textContent = String(n);
+    const n = Math.random();
+    if (n < 0.05) {
+      this.textContent = "大吉"; // 5%
+    } else if (n < 0.2) {
+      this.textContent = "中吉"; //15%
+    } else {
+      this.textContent = "凶"; //80%
+    }
+
+    //     const results = ["大吉", "中吉", "凶", "末吉"];
+    //     const n = Math.floor(Math.random() * results.length);
+    // this.textContent = results[n];
   });
   btn.addEventListener("mousedown", function() {
     this.className = "pushed";
