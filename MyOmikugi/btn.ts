abcc026be47560a6b4a0a58896f714a8bce1d4e5
ririko <ririkokanikama25@yahoo.co.jp>
2018-01-20 01:42:45 +0900

    ドットインストールのおみくじ

--- MyOmikugi/btn.ts
+++ MyOmikugi/btn.ts
@@ -3,22 +3,30 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+//nには0~1までの数字を渡す
+/**
+ * おみくじけっかを返す
+ * @param n 0-1の数字
+ */
+function omikuji(n: number): string {
+  if (n < 0.05) {
+    return "大吉"; // 5%
+  } else if (n < 0.2) {
+    return "中吉"; //15%
+  } else {
+    return "凶"; //80%
+  }
+  //     const results = ["大吉", "中吉", "凶", "末吉"];
+  //     const n = Math.floor(Math.random() * results.length);
+  // this.textContent = results[n];
+}
+
 (function() {
   "use strict";
   const btn = document.getElementById("btn");
   btn.addEventListener("click", function() {
     const n = Math.random();
-    if (n < 0.05) {
-      this.textContent = "大吉"; // 5%
-    } else if (n < 0.2) {
-      this.textContent = "中吉"; //15%
-    } else {
-      this.textContent = "凶"; //80%
-    }
-
-    //     const results = ["大吉", "中吉", "凶", "末吉"];
-    //     const n = Math.floor(Math.random() * results.length);
-    // this.textContent = results[n];
+    this.textContent = omikuji(n);
   });
   btn.addEventListener("mousedown", function() {
     this.className = "pushed";
